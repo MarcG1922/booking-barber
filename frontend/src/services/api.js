fa95bc180180a2eafd015544a7af856020bfab0d
Marc Granero <magrba@floridauniversitaria.es>
2026-07-11 16:30:14 +0200

    Add Axios authentication interceptors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
@@ -14,5 +14,18 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
